5d2088be563dbbe9539ce9640c044911ad2496ad
Sebastian Kolosa <skolosa@pagerduty.com>
2018-12-12 14:50:48 -0800

    initial project setup

---
+++ CleanMeme/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanMeme
+//
+//  Created by Sebastian Kolosa on 12/12/18.
+//  Copyright © 2018 Sebastian Kolosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
